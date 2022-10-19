d8724738f36e4952216232c7d9a03113a339d379
Jose Alonso Espinares Romero <alonso.espinares.socasa.com.mx>
2022-10-19 09:13:10 -0500

    update descarga kardex

--- app/Exports/KardexProductoExport.php
+++ app/Exports/KardexProductoExport.php
@@ -4,6 +4,7 @@ namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Helpers;
 use DB;
 
 class KardexProductoExport implements FromArray, WithHeadings
@@ -28,6 +29,33 @@ class KardexProductoExport implements FromArray, WithHeadings
     {
         $kardex = DB::select('exec ObtenerKardex ?,?', array($this->codigo,$this->almacen));
 
-        return $kardex;
+        $nummovimiento = 1;
+        $entradas = 0;
+        $salidas = 0;
+        $existencias = 0;
+        $numerodecimalesconfigurados = config('app.numerodedecimales');
+        $data = array();
+        foreach(array_reverse($kardex) as $k){
+            $entradas = $entradas + $k->Entradas;
+            $salidas = $salidas + $k->Salidas;
+            $existencias = $existencias + $k->Entradas - $k->Salidas;
+            $colorfila = '';
+            if($k->Status == 'BAJA'){
+                $colorfila = 'bg-red';
+            }
+            $data[]=array(
+                "documento"=>$k->Documento,
+                "movimiento"=>$k->Movimiento,
+                "fecha"=>Helpers::fecha_espanol($k->Fecha),
+                "almacen" => Helpers::convertirvalorcorrecto($k->Almacen),
+                "entradas"=> Helpers::convertirvalorcorrecto($k->Entradas),
+                "salidas" => Helpers::convertirvalorcorrecto($k->Salidas),
+                "existencias"=> round($existencias, $numerodecimalesconfigurados),
+                "costo"=>Helpers::convertirvalorcorrecto($k->Costo),
+                "status"=>$k->Status
+            );
+            $nummovimiento++;
+        }
+        return $data;
     }
 }
